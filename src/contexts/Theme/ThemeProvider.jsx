f0fad0e1351d2fcb1693cfa7f720fd8f7a8e9052
Bernard <bado.bernard94@gmail.com>
2022-01-03 11:07:14 +0100

    fixing components

--- src/contexts/Theme/ThemeProvider.jsx
+++ src/contexts/Theme/ThemeProvider.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ThemeContext from "./ThemeContext";
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState();
+  const [theme, setTheme] = useState("light");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
