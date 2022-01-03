daf21ed61d22289555029dbee84e08d574483d47
Bernard <bado.bernard94@gmail.com>
2022-01-03 11:06:29 +0100

    adding tests for theme provider

---
+++ src/contexts/Theme/ThemeProvider.test.js
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import ThemeProvider from "./ThemeProvider";
+import ThemeContext from "./ThemeContext";
+
+/**
+ * This components is for the testing purposes only
+ */
+const TestComponent = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const setDarkTheme = () => setTheme("dark");
+
+  return (
+    <button data-testid="test-button" className={theme} onClick={setDarkTheme}>
+      Test Button
+    </button>
+  );
+};
+
+describe("ThemeProvider", () => {
+  it("sets light theme as default", () => {
+    render(
+      <ThemeProvider>
+        <TestComponent />
+      </ThemeProvider>
+    );
+
+    // classname of <button> should be "light"
+    expect(screen.getByTestId("test-button").className).toEqual("light");
+  });
+
+  it("changes theme", () => {
+    render(
+      <ThemeProvider>
+        <TestComponent />
+      </ThemeProvider>
+    );
+
+    // Clicking on button will change theme to dark
+    fireEvent.click(screen.getByTestId("test-button"));
+
+    // classname of <button> should be "dark"
+    expect(screen.getByTestId("test-button").className).toEqual("dark");
+  });
+});
